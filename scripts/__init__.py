67492c69dbc8bce3bef922edfec163349347737e
Olaru Marius-Alexandru <marius.alexandru.olaru@gmail.com>
2025-01-04 23:29:20 +0200

    Implemented and trained the Convolutional DQN

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,3 +1,10 @@
-from .dqn import DQN
 from .experience_replay import ReplayMemory
+
+# from .dqn import DQN
+from .dueling_dqn import DuelingDQN
 from .agent import Agent
+
+# from .cnn import CNN
+from .dueling_cnn import DuelingCNN
+from .frame_staker import FrameStacker
+from .agent_2 import Agent2
--- scripts/agent_2.py
+++ scripts/agent_2.py
@@ -3,31 +3,35 @@ import gymnasium
 import torch
 import itertools
 import random
-from scripts import DQN
-from scripts import ReplayMemory
+
+from scripts import DuelingCNN, ReplayMemory, FrameStacker
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Device: {device}")
 
 
-class Agent:
+class Agent2:
     def __init__(self, model_to_test):
 
         # Hyperparameters
         self.replay_memory_size = 200000
-        self.mini_batch_size = 64
+        self.mini_batch_size = 84
         self.epsilon_init = 1
         self.epsilon_decay = 0.998
         self.epsilon_min = 0.1
         self.network_sync_rate = 100
         self.learning_rate_a = 0.001
-        self.discount_factor_g = 0.99
+        self.discount_factor_g = 0.999
         self.stop_on_reward = 500
         self.best_reward = 5
 
+        self.enable_double_dqn = True
+
+        self.image_stack_dimension = 4
         self.model_to_test = model_to_test
 
         self.loss_fn = torch.nn.SmoothL1Loss()
+        # self.loss_fn = torch.nn.MSELoss()
         self.optimizer = None
         self.scheduler = None
 
@@ -38,9 +42,8 @@ class Agent:
     def run(self, is_training=True, render=False):
         env = gymnasium.make("FlappyBird-v0", render_mode="human" if render else None)
 
-        num_states = env.observation_space.shape[0]
         num_actions = env.action_space.n
-        policy_dqn = DQN(in_layer_dim=num_states, out_layer_dim=num_actions).to(device)
+        policy_dqn = DuelingCNN(input_channels=self.image_stack_dimension, input_size=64, out_layer_dim=2).to(device)
 
         if not is_training:
             policy_dqn.load_state_dict(torch.load(self.model_to_test))
@@ -50,8 +53,8 @@ class Agent:
             memory = ReplayMemory(self.replay_memory_size)
             epsilon = self.epsilon_init
 
-            # Duplicate the DQN for target_dqn where we gonna compute the Q values
-            target_dqn = DQN(in_layer_dim=num_states, out_layer_dim=num_actions).to(device)
+            # Duplicate the DQN for target_dqn where we are going to compute the Q values
+            target_dqn = DuelingCNN(input_channels=self.image_stack_dimension, input_size=64, out_layer_dim=2).to(device)
             # Copy the w and b at target_dqn from policy_dqn
             target_dqn.load_state_dict(policy_dqn.state_dict())
 
@@ -61,10 +64,13 @@ class Agent:
 
         reward_per_episode = []
         epsilon_history = []
+        frame_stacker = FrameStacker(stack_size=self.image_stack_dimension, height=64, width=64)
 
         for episode in itertools.count():
             state, _ = env.reset()
-            state = torch.tensor(state, dtype=torch.float, device=device)
+            stacked_frames = frame_stacker.reset(state)
+            state = torch.tensor(stacked_frames, dtype=torch.float, device=device)
+
             terminated = False
             episode_reward = 0.0
 
@@ -73,14 +79,16 @@ class Agent:
                 if is_training and random.random() < epsilon:
                     action = env.action_space.sample()
                 else:
-                    with torch.no_grad():
+                    with torch.no_grad():  # ??
                         action = policy_dqn(state.unsqueeze(dim=0)).squeeze().argmax().item()
+                        # action = policy_dqn(state.unsqueeze(dim=0)).argmax().item()
 
                 # Environment step
-                new_state, reward, terminated, _, info = env.step(action)
+                new_frame, reward, terminated, _, info = env.step(action)
+                stacked_frames = frame_stacker.update(new_frame)
                 episode_reward += reward
 
-                new_state = torch.tensor(new_state, dtype=torch.float, device=device)
+                new_state = torch.tensor(stacked_frames, dtype=torch.float, device=device)
                 reward = torch.tensor(reward, dtype=torch.float, device=device)
 
                 if is_training:
@@ -108,7 +116,7 @@ class Agent:
 
             if episode_reward > self.best_reward:
                 self.best_reward = episode_reward
-                torch.save(policy_dqn.state_dict(), f"./best_models/trained_q_function_{self.best_reward:.3f}.pth")
+                torch.save(policy_dqn.state_dict(), f"./best_models_CNN/DuelingCNN/trained_q_function_{self.best_reward:.3f}.pth")
                 print(f"Model with best reward of {episode_reward} saved at episode {episode}.")
 
             print(f"Episode: {episode}, Reward: {episode_reward}, Epsilon: {epsilon:.3f}")
@@ -122,25 +130,36 @@ class Agent:
         """
         Q-Learning Formula:
             q[state, action] = lr * (reward + discount_factor * max(q[new_state,:]) - q[state, action])
-
+        ---------------------------------------------------------------------------------
         DQN Target Formula:
-            q[state, action] = reward if new_state is terminal else
-                               reward + discount_factor * max(q[new_state,:])
+            Qt[state, action] = reward if new_state is terminal else
+                               reward + discount_factor * max(Qt[new_state,:])
+        ---------------------------------------------------------------------------------
+        Double DQN Target Formula:
+            best_action = arg(max(Qp[new_state,:]))
+            Qt[state, action] = reward if new_state is terminal else
+                               reward + discount_factor * max(Qt[best_action])
         """
-        states, actions, new_states, rewards, terminations = zip(*mini_batch)
+        frames, actions, new_frames, rewards, terminations = zip(*mini_batch)
 
-        states = torch.stack(states)
+        frames = torch.stack(frames)
         actions = torch.tensor(actions, dtype=torch.long, device=device)
-        new_states = torch.stack(new_states)
+        new_frames = torch.stack(new_frames)
         rewards = torch.stack(rewards)
         terminations = torch.tensor(terminations, dtype=torch.float, device=device)
 
         with torch.no_grad():
-            """DQN Target Formula"""
-            target_q = rewards + (1 - terminations) * self.discount_factor_g * target_dqn(new_states).max(dim=1)[0]
+            if self.enable_double_dqn:
+                """Double DQN Target Formula"""
+                best_action_from_policy = policy_dqn(new_frames).argmax(dim=1)
+                target_q = rewards + (1 - terminations) * self.discount_factor_g * target_dqn(new_frames).gather(dim=1,
+                                                                                                                 index=best_action_from_policy.unsqueeze(dim=1)).squeeze()
+            else:
+                """DQN Target Formula"""
+                target_q = rewards + (1 - terminations) * self.discount_factor_g * target_dqn(new_frames).max(dim=1)[0]
 
         # Compute Q values from policy
-        current_q = policy_dqn(states).gather(dim=1, index=actions.unsqueeze(dim=1)).squeeze()
+        current_q = policy_dqn(frames).gather(dim=1, index=actions.unsqueeze(dim=1)).squeeze()
         loss = self.loss_fn(current_q, target_q)
 
         self.optimizer.zero_grad()
